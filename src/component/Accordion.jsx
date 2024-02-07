25006731e1de328041ace76e46a90afbac215795
Nahid <allahparastnahid@gmail.com>
2024-02-07 10:09:18 +0330

    state lifted and in one time only one accordion is open

--- src/component/Accordion.jsx
+++ src/component/Accordion.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import{ChevronDownIcon}from '@heroicons/react/24/outline'
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
 
 const data = [
   {
@@ -18,11 +18,21 @@ const data = [
     text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt molestias asperiores dolorem, beatae eius distinctio quis labore officia, numquam libero autem iste dolores id unde expedita excepturi? Accusamus, commodi harum",
   },
 ];
-const Accordion = ({}) => {
+const Accordion = () => {
+  const [open, setOpen] = useState(null);
+  const openHandler = (id) => {
+    setOpen(id === open ? null : id);
+  };
+
   return (
     <div className="accardion">
       {data.map((item) => (
-        <AccordionItem key={item.id} item={item} />
+        <AccordionItem
+          key={item.id}
+          item={item}
+          onOpen={openHandler}
+          open={open}
+        />
       ))}
     </div>
   );
@@ -30,13 +40,13 @@ const Accordion = ({}) => {
 
 export default Accordion;
 
-const AccordionItem = ({ item }) => {
-  const [isOpen, setIsOpen] = useState(false);
+const AccordionItem = ({ item, open, onOpen }) => {
+  const isOpen = item.id === open;
   return (
     <div className={`accordion-item ${isOpen && "accordion__expanded"}`}>
-      <div className="accordion-item__header" onClick={()=>setIsOpen(!isOpen)}>
-       <div> {item.title}</div>
-       <ChevronDownIcon className="accordion-item__chevron"/>
+      <div className="accordion-item__header" onClick={() => onOpen(item.id)}>
+        <div> {item.title}</div>
+        <ChevronDownIcon className="accordion-item__chevron" />
       </div>
       <div className="accordion-item__content">{item.text}</div>
     </div>
